9ac25531ea65e59a1c401c9bac6e0a2910b33579
teddylakoski <t.j.lakoski4@tcu.edu>
2026-02-17 16:35:10 -0600

    Update StreamlitApp_Week_3.py

--- Portfolio/StreamlitApp_Week_3.py
+++ Portfolio/StreamlitApp_Week_3.py
@@ -59,7 +59,7 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer.shap',
         "pipeline": 'finalized_model.tar.gz',
-        "keys": ["AAPL", "IBM", "GOOGL", "AMD", "SP500"],
+        "keys": ["NVDA", "AVGO", "TSM", "ORCL", "AMD", "META"],
         "inputs": [{"name": k, "type": "number", "min": -1.0, "max": 1.0, "default": 0.0, "step": 0.01} for k in ["AAPL", "IBM", "GOOGL", "AMD", "SP500"]]
 }
 
@@ -151,6 +151,7 @@ if submitted:
         display_explanation(input_df,session, aws_bucket)
     else:
         st.error(res)
+
 
 
 
