70ef3b952c2859d2a0b73864000c71db3024dc2d
teddylakoski <t.j.lakoski4@tcu.edu>
2026-03-26 17:00:21 -0500

    Update model keys and inputs in Streamlit app

--- Portfolio/StreamlitApp_Week_7.py
+++ Portfolio/StreamlitApp_Week_7.py
@@ -60,8 +60,8 @@ MODEL_INFO = {
         "endpoint": aws_endpoint,
         "explainer": 'explainer_pair.shap',
         "pipeline": 'finalized_pair_model.tar.gz',
-        "keys": ["MPWR", "AAPL"],
-        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["MPWR", "AAPL"]]
+        "keys": ["NVDA", "ANET"],
+        "inputs": [{"name": k, "type": "number", "min": 0.0, "default": 0.0, "step": 10.0} for k in ["NVDA", "ANET"]]
 }
 
 def load_pipeline(_session, bucket, key):
